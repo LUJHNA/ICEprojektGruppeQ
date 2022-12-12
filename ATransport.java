61c8098b0acf66b96f816e938ffbf86204b44792
DimitraSis <dimitra.sis7@gmail.com>
2022-12-12 11:07:49 +0100

    class ATransport

---
+++ ATransport.java
@@ -0,0 +1,10 @@
+public abstract class ATransport {
+
+    public String station;
+    public int zone;
+
+    public ATransport(String station, int zone) {
+        this.station = station;
+        this.zone = zone;
+    }
+}
