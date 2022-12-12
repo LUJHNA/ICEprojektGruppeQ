2fd20d4cb34269633126f8c36faa4352ced11ca4
MortenNie <cph-mn705@cphbusiness.dk>
2022-12-12 09:12:17 +0100

    update

--- TravelStart.java
+++ TravelStart.java
@@ -1,8 +1,9 @@
 public class TravelStart {
 
     public static void main(String[] args) {
-
-
+        FileIO fileio = new FileIO();
+        fileio.makeRoute("a", "c");
+        fileio.loginAndRegister2();
 
 
     }
