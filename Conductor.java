f312f0e57a18a626d267688477f0aa505ab7576c
LUJHNA <Jakob_h_95@hotmail.com>
2022-12-14 08:40:09 +0100

    update conductor
    hehe

---
+++ Conductor.java
@@ -0,0 +1,26 @@
+import java.sql.SQLOutput;
+import java.util.Random;
+
+public class Conductor {
+
+
+
+    public double conductorChance(User us){
+        Random chance = new Random();
+        int conductorChance = chance.nextInt(5); //1/6 chance for Conductor to enter.
+
+        if(!us.checkInState && conductorChance == 0){
+            System.out.println("You've been caught crime riding!(Traveling without TravelCard)");
+            System.out.println("You've received a fine of 750KR.");
+            int fine = 750;
+            return us.travelcard.getBalance() - fine;
+        } else if(us.checkInState && conductorChance == 0){
+            System.out.println("A conductor enters and checks your ticket. All is okay");
+        }
+
+
+    return us.travelcard.getBalance();
+    }
+
+
+}
